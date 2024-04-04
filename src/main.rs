d7607b5e1ad2c0bf8dd1646e2b8af3dd6af915f1
DeTuksa <emmydave414@gmail.com>
2024-04-04 13:16:16 +0100

    Feat: Added gui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod gui;
+
+use gtk::prelude::*;
+use gtk::Application;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+    .application_id("com.asas.stickynotes")
+    .build();
+
+    app.connect_activate(|app| {
+        gui::build_ui(&app)
+    });
+
+    app.run();
 }
